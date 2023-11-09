a9b4ce90d33c95b04a342c9e5ac8113a609c80bc
Виктор Баранов <vitya.ak22071990@gmail.com>
2023-11-09 19:56:50 +0200

    use Illuminate\Support\Facades\Cache;

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,6 +5,7 @@ use App\Models\Comment;
 use App\Rules\AllowedHtmlTags;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Storage;
 
